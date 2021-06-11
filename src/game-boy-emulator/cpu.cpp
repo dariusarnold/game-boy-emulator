edae9a5e03e4c9ce0564075fd346700c0da69ed6
Darius Arnold <mail@dariusarnold.de>
2021-06-11 21:52:04 +0200

    Bug fix: Increment program counter in reset bit

--- src/game-boy-emulator/cpu.cpp
+++ src/game-boy-emulator/cpu.cpp
@@ -296,6 +296,7 @@ uint8_t Cpu::cb(opcodes::OpCode op_code) {
     } else if (op_code.value >= opcodes::RES_0B.value && op_code.value <= opcodes::RES_7A.value) {
         // Reset bit instruction
         reset_bit(op_code_to_register(op_code), internal::op_code_to_bit(op_code));
+        registers.pc++;
         return 4;
     } else if (op_code.value >= opcodes::SET_0B.value && op_code.value <= opcodes::SET_7A) {
         // Set bit instruction

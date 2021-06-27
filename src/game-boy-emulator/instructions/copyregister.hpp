a989c269d463a50cb7f047d7df420ad8b1aadc79
Darius Arnold <mail@dariusarnold.de>
2021-06-27 20:29:31 +0200

    Fix parentheses warning

--- src/game-boy-emulator/instructions/copyregister.hpp
+++ src/game-boy-emulator/instructions/copyregister.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "facades/register.hpp"
 #include "facades/incrementpc.hpp"
+#include "facades/register.hpp"
 
 /**
  * Copy value from input register to output register.
@@ -11,15 +11,15 @@
 template <typename I, typename O>
 class CopyRegister {
     // Check if same register type
-    static_assert(registers::is_byte_register_v<I> and registers::is_byte_register_v<O> or registers::is_word_register_v<I> and registers::is_word_register_v<O>);
+    static_assert((registers::is_byte_register_v<I> and registers::is_byte_register_v<O>)
+                  or (registers::is_word_register_v<I> and registers::is_word_register_v<O>));
     Register<I> source;
     MutableRegister<O> destination;
     IncrementPC increment_pc;
 
 public:
     CopyRegister(Register<I> sourc, MutableRegister<O> dest, IncrementPC ipc) :
-            source(sourc), destination(dest), increment_pc(ipc) {
-    }
+            source(sourc), destination(dest), increment_pc(ipc) {}
 
     unsigned int execute() {
         destination.set(source.get());

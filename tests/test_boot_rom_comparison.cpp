794de19338bb06886219aa753c3836bde5ea9748
Darius Arnold <mail@dariusarnold.de>
2022-01-15 23:41:53 +0100

    Improve boot rom comparison test output by printing expected and actual values horizontally aligned

--- tests/test_boot_rom_comparison.cpp
+++ tests/test_boot_rom_comparison.cpp
@@ -25,6 +25,27 @@ double calculate_percentage(double part, double total) {
     return part / total * 100.0;
 }
 
+// This matcher prints both the actual and the expected results directly below each other so that
+// each actual value is aligned with the expected value. This makes it easier to spot differences.
+class DebugStringEqual: public Catch::MatcherBase<std::string> {
+    std::string m_expected_result;
+public:
+    explicit DebugStringEqual(std::string expected_result): m_expected_result(std::move(expected_result)) {}
+
+    bool match(const std::string& actual_result) const override {
+        return actual_result == m_expected_result;
+    }
+
+    std::string describe() const override {
+        return fmt::format("\n  \"{}\" (expected)", m_expected_result);
+    }
+
+};
+
+inline DebugStringEqual StringEqualAlignedOutput(std::string expected_result) {
+    return DebugStringEqual(std::move(expected_result));
+}
+
 TEST_CASE("Compare boot sequence") {
     auto expected_output = read_bootrom_log_file();
     REQUIRE_FALSE(expected_output.empty());
@@ -37,7 +58,7 @@ TEST_CASE("Compare boot sequence") {
         auto actual_output = cpu.get_minimal_debug_state();
         INFO(fmt::format("Executing instruction number {}/{} ({:.2f}% done)", i, expected_output.size(), calculate_percentage(i, expected_output.size())));
         ++i;
-        REQUIRE_THAT(actual_output,  Catch::Matchers::Equals(expected_line));
+        REQUIRE_THAT(actual_output, StringEqualAlignedOutput(expected_line));
         cpu.step();
     }
 }

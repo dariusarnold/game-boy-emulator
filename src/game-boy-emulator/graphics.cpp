f3bc05fd4d32165610407b7535aa58ce655cd94c
Darius Arnold <mail@dariusarnold.de>
2022-11-19 19:03:22 +0100

    Remove unused dependency stb
    This was used for trying out dear-imgui and is not required anymore.

--- src/game-boy-emulator/graphics.cpp
+++ src/game-boy-emulator/graphics.cpp
@@ -3,45 +3,8 @@
 
 #include "SDL.h"
 #include "SDL_opengl.h"
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
 
 namespace graphics::render {
-
-bool load_texture_from_file(const char* filename, GLuint* out_texture, int* out_width,
-                         int* out_height) {
-    // Load from file
-    int image_width = 0;
-    int image_height = 0;
-    unsigned char* image_data = stbi_load(filename, &image_width, &image_height, NULL, 4);
-    if (image_data == nullptr) return false;
-
-    // Create a OpenGL texture identifier
-    GLuint image_texture;
-    glGenTextures(1, &image_texture);
-    glBindTexture(GL_TEXTURE_2D, image_texture);
-
-    // Setup filtering parameters for display
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,
-                    GL_CLAMP_TO_EDGE); // This is required on WebGL for non power-of-two textures
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE); // Same
-
-    // Upload pixels into texture
-#if defined(GL_UNPACK_ROW_LENGTH) && !defined(__EMSCRIPTEN__)
-    glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
-#endif
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE,
-                 image_data);
-    stbi_image_free(image_data);
-
-    *out_texture = image_texture;
-    *out_width = image_width;
-    *out_height = image_height;
-
-    return true;
-}
 
 void load_texture_rgba(const uint32_t* data, int width, int height, GLuint* out_texture) {
     // Create a OpenGL texture identifier

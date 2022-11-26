2877737dd7c05401ea13b444ba68a6a53e3f95f2
Darius Arnold <mail@dariusarnold.de>
2022-11-26 22:03:33 +0100

    Hardcode cartridge header for boot rom test
    Booting requires the cartridge header to pass the checksum test,
    otherwise the game boy will lock up.

--- tests/test_boot_rom_comparison.cpp
+++ tests/test_boot_rom_comparison.cpp
@@ -1,7 +1,7 @@
 #include "catch2/catch.hpp"
 
-#include "io.hpp"
 #include "cpu.hpp"
+#include "io.hpp"
 
 #include "apu.hpp"
 #include "emulator.hpp"
@@ -31,10 +31,12 @@ double calculate_percentage(double part, double total) {
 
 // This matcher prints both the actual and the expected results directly below each other so that
 // each actual value is aligned with the expected value. This makes it easier to spot differences.
-class DebugStringEqual: public Catch::MatcherBase<std::string> {
+class DebugStringEqual : public Catch::MatcherBase<std::string> {
     std::string m_expected_result;
+
 public:
-    explicit DebugStringEqual(std::string expected_result): m_expected_result(std::move(expected_result)) {}
+    explicit DebugStringEqual(std::string expected_result) :
+            m_expected_result(std::move(expected_result)) {}
 
     bool match(const std::string& actual_result) const override {
         return actual_result == m_expected_result;
@@ -43,7 +45,6 @@ public:
     std::string describe() const override {
         return fmt::format("\n  \"{}\" (expected)", m_expected_result);
     }
-
 };
 
 inline DebugStringEqual StringEqualAlignedOutput(std::string expected_result) {
@@ -55,9 +56,33 @@ TEST_CASE("Compare boot sequence") {
     REQUIRE_FALSE(expected_output.empty());
     auto boot_rom_path = std::filesystem::absolute(("dmg01-boot.bin"));
     auto boot_rom = load_boot_rom_file(boot_rom_path);
+    std::vector<uint8_t> cartridge(1024, 0);
+    // clang-format off
+    const std::vector<uint8_t> cartridge_header{0x00, 0xC3, 0x1, 0x50 /* entry point */,
+                                                0xCE, 0xED, 0x66, 0x66, 0xCC, 0x0D, 0x00, 0x0B, 0x03, 0x73, 0x00, 0x83, 0x00, 0x0C, 0x00,
+                                                0x0D, 0x00, 0x08, 0x11, 0x1F, 0x88, 0x89, 0x00, 0x0E, 0xDC, 0xCC, 0x6E, 0xE6, 0xDD, 0xDD,
+                                                0xD9, 0x99, 0xBB, 0xBB, 0x67, 0x63, 0x6E, 0x0E, 0xEC, 0xCC, 0xDD, 0xDC, 0x99, 0x9F, 0xBB,
+                                                0xB9, 0x33, 0x3E /* logo */,
+                                                0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0 /* game title (should be 16 bytes, but the boot rom log only uses 15 */,
+                                                0x80 /* cgb flag*/,
+                                                0x0,
+                                                0x0 /* licensee code */,
+                                                0x0 /* sgb flag*/,
+                                                0x1 /* cartridge type */,
+                                                0x0 /* rom size*/,
+                                                0x0 /* ram size */,
+                                                0x0 /* destination code */,
+                                                0x0 /* old licensee code */,
+                                                0x0 /* version number */,
+                                                0x66 /* header checksum */,
+                                                0x0, 0x0 /* global checksum */};
+    // clang-format on
+    for (auto i = 0; i < cartridge_header.size(); ++i) {
+        cartridge[i + 0x100] = cartridge_header[i];
+    }
     REQUIRE(boot_rom);
-    Emulator emulator{boot_rom.value(), {}};
-    for (auto i = 0; const auto& expected_line: expected_output) {
+    Emulator emulator{boot_rom.value(), cartridge};
+    for (auto i = 0; const auto& expected_line : expected_output) {
         auto actual_output = emulator.get_cpu_debug_state();
         INFO(fmt::format("Executing instruction number {}/{} ({:.2f}% done. Last instructions: {} "
                          "{}.\n CPU state: {})",

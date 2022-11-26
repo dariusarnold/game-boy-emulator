0bb0657431efcea833fbc00e0a0a2fdd3ea9d425
Darius Arnold <mail@dariusarnold.de>
2022-11-26 22:01:21 +0100

    fixme io register

--- src/game-boy-emulator/addressbus.cpp
+++ src/game-boy-emulator/addressbus.cpp
@@ -11,6 +11,8 @@
 
 AddressBus::AddressBus(Emulator* emulator) : m_emulator(emulator) {}
 
+auto io_registerFF42 = 0x64;
+
 uint8_t AddressBus::read_byte(uint16_t address) const {
     if (m_emulator->is_booting() && memmap::isIn(address, memmap::BootRom)) {
         return m_emulator->get_boot_rom()->read_byte(address);
@@ -25,14 +27,13 @@ uint8_t AddressBus::read_byte(uint16_t address) const {
                || memmap::isIn(address, memmap::CharacterRam)) {
         return m_emulator->get_gpu()->read_byte(address);
     } else if (memmap::isIn(address, memmap::IORegisters)) {
-        fmt::print("IGNORED: read from IO register {:04X}\n", address);
         // TODO Those special cases are required for booting correctly
         if (address == 0xFF44) {
             return 0x90;
         } else if (address == 0xFF42) {
-            static uint8_t value = 0x64;
-            return value--;
+            return io_registerFF42;
         }
+        fmt::print("IGNORED: read from IO register {:04X}\n", address);
         return 0xFF;
     }
     throw NotImplementedError(fmt::format("Addressing unmapped memory byte at {:04X}", address));
@@ -44,6 +45,9 @@ void AddressBus::write_byte(uint16_t address, uint8_t value) {
     } else if (memmap::isIn(address, memmap::VRam)) {
         m_emulator->get_gpu()->write_byte(address, value);
     } else if(memmap::isIn(address, memmap::IORegisters)) {
+        if (address == 0xFF42) {
+            io_registerFF42 = value;
+        }
         fmt::print("IGNORED: write to IO registers {:04X}\n", address);
     } else {
         throw NotImplementedError(fmt::format("Writing unmapped memory byte at {:04X}", address));

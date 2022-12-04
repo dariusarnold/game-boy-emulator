68d2f1e08a5e79de74d3474324150c123d5a3337
Darius Arnold <mail@dariusarnold.de>
2022-12-04 22:17:15 +0100

    Add blargg test rom 2 for interrupts as test case
    Former-commit-id: e1e4baf2380f7b21fba789153575a7f857642a1d [formerly 50ed7a79e9164d7cfbc98a0cd34748b17a00390a]
    Former-commit-id: b2ad378c936ff79a8f3ac84b7b97ca7e2fb9b420

---
+++ src/tests/test_blargg2_comparison.cpp
@@ -0,0 +1,30 @@
+#include "catch2/catch.hpp"
+
+#include "cpu.hpp"
+#include "io.hpp"
+#include "emulator.hpp"
+#include "gpu.hpp"
+
+#include "test_helpers.hpp"
+#include "spdlog/spdlog.h"
+
+#include <filesystem>
+
+TEST_CASE("Compare blargg2 state") {
+    spdlog::set_level(spdlog::level::err);
+    auto expected_output = read_log_file("blargg2.txt");
+    REQUIRE_FALSE(expected_output.empty());
+    auto boot_rom_path = std::filesystem::absolute(("dmg01-boot.bin"));
+    auto boot_rom = load_boot_rom_file(boot_rom_path);
+    auto blargg_rom = load_rom_file(std::filesystem::absolute("blargg2.gb"));
+    REQUIRE(boot_rom);
+    spdlog::set_level(spdlog::level::trace);
+    Emulator emulator{blargg_rom};
+    for (auto i = 0; const auto& expected_line : expected_output) {
+        auto actual_output = emulator.get_debug_state();
+        ++i;
+        INFO("Line " << i << " " << static_cast<double>(i) / expected_output.size() << " %");
+        REQUIRE(actual_output == expected_line);
+        REQUIRE(emulator.step());
+    }
+}

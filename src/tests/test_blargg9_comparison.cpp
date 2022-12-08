a09c9ca6e09c8a5445685cc0f63a0e05b84e53ae
Darius Arnold <mail@dariusarnold.de>
2022-12-08 20:33:53 +0100

    Add blargg test rom 9
    Former-commit-id: 776b0cc7035b6fc9e5bafd6366a7fd47fa09d7e9 [formerly 6475552645e828758a924ebf75609730779eec94]
    Former-commit-id: 1657c730fb0c07ccb7406fc771d4ea4987d7577d

---
+++ src/tests/test_blargg9_comparison.cpp
@@ -0,0 +1,26 @@
+#include "catch2/catch.hpp"
+
+#include "cpu.hpp"
+#include "io.hpp"
+#include "emulator.hpp"
+#include "gpu.hpp"
+
+#include "test_helpers.hpp"
+#include "spdlog/spdlog.h"
+
+#include <filesystem>
+
+TEST_CASE("Compare blargg9 state") {
+    spdlog::set_level(spdlog::level::err);
+    auto expected_output = read_log_file("recorded-logs/blargg9.txt");
+    REQUIRE_FALSE(expected_output.empty());
+    auto blargg_rom = load_rom_file(std::filesystem::absolute("roms/09-op r,r.gb"));
+    Emulator emulator{blargg_rom};
+    for (auto i = 0; const auto& expected_line : expected_output) {
+        auto actual_output = emulator.get_debug_state();
+        ++i;
+        INFO("Line " << i << " " << static_cast<double>(i) / expected_output.size() * 100 << " %");
+        REQUIRE(actual_output == expected_line);
+        REQUIRE(emulator.step());
+    }
+}

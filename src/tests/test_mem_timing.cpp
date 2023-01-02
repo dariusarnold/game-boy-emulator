808c145981c6d1738f8a7a8f44bbd42766976872
Darius Arnold <mail@dariusarnold.de>
2023-01-02 13:38:19 +0100

    Add blargg mem_timing test

---
+++ src/tests/test_mem_timing.cpp
@@ -0,0 +1,22 @@
+#include "catch2/catch.hpp"
+
+#include "emulator.hpp"
+#include "ppu.hpp"
+#include "serial_port.hpp"
+
+#include "test_helpers.hpp"
+#include "spdlog/spdlog.h"
+
+#include <filesystem>
+
+TEST_CASE("blargg mem_timing") {
+    spdlog::set_level(spdlog::level::err);
+    Emulator emulator{{true}};
+    emulator.load_game(std::filesystem::absolute("roms/mem_timing.gb"));
+    for (auto i = 0; i <= 813000; ++i) {
+        REQUIRE(emulator.step());
+    }
+    auto serial_content = emulator.get_serial_port()->get_buffer();
+    INFO("Serial buffer " << serial_content);
+    REQUIRE(serial_content.find("Passed") != std::string::npos);
+}

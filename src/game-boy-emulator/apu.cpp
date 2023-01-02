58679b16e74f61be4f774f3de62bd0c04d8ee000
Darius Arnold <mail@dariusarnold.de>
2023-01-02 11:28:06 +0100

    Add read/write registers to APU
    Some games (such as Pokemon Red) expect working audio registers. Pokemon
    Red will hang when using a PokeCenter otherwise.

--- src/game-boy-emulator/apu.cpp
+++ src/game-boy-emulator/apu.cpp
@@ -1,14 +1,27 @@
 #include "apu.hpp"
 #include "spdlog/spdlog.h"
+#include "memorymap.hpp"
 
 
-Apu::Apu(): m_logger(spdlog::get("")) {}
+Apu::Apu() : m_logger(spdlog::get("")) {}
 
 uint8_t Apu::read_byte(uint16_t address) {
+    if (memmap::is_in(address, memmap::Apu)) {
+        return m_register_block1[address - memmap::ApuBegin];
+    }
+    if (memmap::is_in(address, memmap::WavePattern)) {
+        return m_register_block2[address - memmap::WavePatternBegin];
+    }
     m_logger->debug("APU: Unhandled read at {:04X}", address);
     return 0xFF;
 }
 
-void Apu::write_byte(uint16_t address, uint8_t) {
+void Apu::write_byte(uint16_t address, uint8_t value) {
+    if (memmap::is_in(address, memmap::Apu)) {
+        m_register_block1[address - memmap::ApuBegin] = value;
+    }
+    if (memmap::is_in(address, memmap::WavePattern)) {
+        m_register_block2[address - memmap::WavePatternBegin] = value;
+    }
     m_logger->debug("APU: Unhandled write at {:04X}", address);
 }
--- src/game-boy-emulator/apu.hpp
+++ src/game-boy-emulator/apu.hpp
@@ -5,9 +5,15 @@ class logger;
 }
 #include <memory>
 #include <cstdint>
+#include <array>
 
 class Apu {
     std::shared_ptr<spdlog::logger> m_logger;
+
+    // From 0xFF10-0xFF26
+    std::array<uint8_t, 23> m_register_block1{};
+    // From 0xFF30-0xFF3F
+    std::array<uint8_t, 16> m_register_block2{};
 
 public:
     Apu();

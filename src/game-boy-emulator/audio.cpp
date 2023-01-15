b822955416a7acac2fa18e7528e8c1c82709af35
Darius Arnold <mail@dariusarnold.de>
2023-01-15 22:22:49 +0100

    Add console output which audio device was selected

--- src/game-boy-emulator/audio.cpp
+++ src/game-boy-emulator/audio.cpp
@@ -10,9 +10,11 @@ constexpr int BUFFER_SIZE = 4096;
 
 Audio::Audio() : m_resampler(AUDIO_F32SYS, AUDIO_F32SYS, constants::CLOCK_SPEED_M, 44100, 2, 2) {
     auto num_audio_devices = SDL_GetNumAudioDevices(0);
+    std::vector<const char*> device_ids(static_cast<size_t>(num_audio_devices));
     for (auto i = 0; i < num_audio_devices; ++i) {
         // TODO Implement device selection by user
         const auto* name = SDL_GetAudioDeviceName(i, SDL_AUDIO_PLAYBACK);
+        device_ids[static_cast<size_t>(i)] = name;
         spdlog::info("Detected audio device {}: {}", i, name);
     }
 
@@ -22,8 +24,8 @@ Audio::Audio() : m_resampler(AUDIO_F32SYS, AUDIO_F32SYS, constants::CLOCK_SPEED_
     audio_spec.samples = BUFFER_SIZE;
     audio_spec.channels = 2;
     SDL_AudioSpec obtained{};
-    m_device_id = SDL_OpenAudioDevice(SDL_AUDIO_DEFAULT_DEVICE, SDL_AUDIO_PLAYBACK, &audio_spec,
-                                      &obtained, 0);
+    spdlog::info("Using audio device {}", device_ids[0]);
+    m_device_id = SDL_OpenAudioDevice(device_ids[0], SDL_AUDIO_PLAYBACK, &audio_spec, &obtained, 0);
     assert(obtained.freq == audio_spec.freq && "Audio device configuration mismatch");
     if (m_device_id == 0) {
         spdlog::error("Failed to open audio: {}", SDL_GetError());

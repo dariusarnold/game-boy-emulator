9ece342a66d854853199889ec0271ead8907481e
Darius Arnold <mail@dariusarnold.de>
2023-03-29 22:57:30 +0200

    Refactor to a modern conan integration
    The CMakeToolchain and CMakeDeps are the recommended generators and are
    supported in Conan 2.0 as well.

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,25 @@
-from conans import ConanFile, CMake
+from conans import ConanFile
+from conan.tools.cmake import cmake_layout
+
 
 class GameBoyEmulatorConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "fmt/9.1.0", "catch2/2.13.6", "imgui/1.88", "magic_enum/0.8.1", "spdlog/1.11.0", "argparse/2.9", "boost/1.80.0", "sdl/2.26.1", ("nas/1.9.4", "override")
-    generators = "cmake_find_package", "cmake_paths"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def requirements(self):
+        self.requires("fmt/9.1.0")
+        self.requires("imgui/1.88")
+        self.requires("magic_enum/0.8.1")
+        self.requires("spdlog/1.11.0")
+        self.requires("argparse/2.9")
+        self.requires("boost/1.80.0")
+        self.requires("sdl/2.26.1")
+        self.requires("catch2/2.13.6")
+        if "arm" in self.settings.arch:
+            self.requires("nas/1.9.4", override=True)
+
+    def layout(self):
+        cmake_layout(self)
 
     def configure(self):
         self.options["boost"].header_only = True

1dc7b9672c6af27fb2c9b461da2b719b7eb75ef7
Darius Arnold <mail@dariusarnold.de>
2026-04-30 18:35:33 +0000

    Rework conan_build.sh helper script
    - Allow specifying build type (debug/release)
    - Rename build dir

--- conan_build.sh
+++ conan_build.sh
@@ -4,15 +4,14 @@
 # Dependencies (outside of those provided by dev container) are installed using conan.
 # 
 # Arguments:
-# 1: Conan Profile: Name of conan profile to use.
+# 1: Conan profile path or name. Defaults to profiles/gcc-14.
+# 2: Conan/CMake build type. Defaults to Release.
 
 
 set -euo pipefail
 
-CONAN_PROFILE="gcc-14"
-if [ $# -gt 0 ]; then
-  CONAN_PROFILE="$1"
-fi
+CONAN_PROFILE="${1:-profiles/gcc-14}"
+BUILD_TYPE="${2:-Release}"
 
-conan install . --output-folder build --build=missing -c tools.cmake.cmaketoolchain:generator=Ninja -pr:a "$CONAN_PROFILE"
-conan build . -pr:a "$CONAN_PROFILE"
+conan install . --output-folder build/conan_out --build=missing -pr:a "$CONAN_PROFILE" -s:a build_type="$BUILD_TYPE"
+conan build . --output-folder build/conan_out -pr:a "$CONAN_PROFILE" -s:a build_type="$BUILD_TYPE"
